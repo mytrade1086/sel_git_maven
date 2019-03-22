6ebf57ee18ac8e4246eefd34786272b1ade02ffa
mytade1086 <mytrade1086@gmail.com>
2019-03-23 01:05:12 +0530

    Adding via UI

---
+++ Sel_git_maven/src/test/java/com/Sel_git_maven/testcases/TestCase.java
@@ -0,0 +1,23 @@
+package com.Sel_git_maven.testcases;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TestCase {
+	
+	
+	@Test
+	public void launch() throws InterruptedException {
+		
+		
+		System.setProperty("webdriver.chrome.driver", "H:\\chromedriver (2).exe");
+		
+		ChromeDriver driver=new ChromeDriver();
+		driver.get("https://www.rediff.com");
+		Thread.sleep(4000);
+		driver.quit();
+		
+		
+	}
+
+}
